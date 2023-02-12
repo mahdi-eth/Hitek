33342994ab22233a5d2372b4c0a41fee21c106b8
Mahdi-Ettehadnejad <mahdi.ettehad85@gmail.com>
2023-02-12 19:58:33 +0330

    Configuring the project

---
+++ view/tailwind.config.cjs
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,jsx}"],
+    theme: {
+        extend: {},
+        colors: {
+            primary: "#1565D8",
+            secandry: "#FAAD13",
+            ـDarkblue: "#183B56",
+            _Gray: "#183B56",
+        },
+        // typography: {
+        //   sans: ,
+        //   serif: ,
+        // },
+    },
+    plugins: []
+};
