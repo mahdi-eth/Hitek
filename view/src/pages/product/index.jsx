8e47b4e54e2f014e769a980c9714adc3e0635edb
mohammad_Dn <mohammaddehghani2677@gmail.com>
2023-02-19 13:52:12 +0330

    add Pages

---
+++ view/src/pages/product/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function ProductPage() {
+  return (
+    <div>productPage</div>
+  )
+}
--- view/src/routes/index.jsx
+++ view/src/routes/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import { ProductPage } from "../pages";
 export const routes = {
   LAUNCHSCREEN: {
     path: "/",
     element: <h1>Test</h1>,
     key: 1
+  },
+  PRODUCTPAGE:{
+    path:'/product',
+    element:<ProductPage/>
+
   }
 };
