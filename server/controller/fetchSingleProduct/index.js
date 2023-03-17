3dc6847f37f4d774c3e7196da188dbc5e2fac60a
mohammad_Dn <mohammaddehghani2677@gmail.com>
2023-03-17 17:20:36 +0330

    update last commit

--- server/controller/fetchSingleProduct/index.js
+++ server/controller/fetchSingleProduct/index.js
@@ -1,6 +1,8 @@
 const products = require("../../model/products");
 
 const fetchProduct =async (req, res) => {
+  console.log(req.body);
+  const productId=req.body
   const singleproduct= await products.findById('640cf37871a98b0151ed6b8a',{})
  res.json({singleproduct})
 }
--- view/src/api/services/fetchSingleProduct/index.js
+++ view/src/api/services/fetchSingleProduct/index.js
@@ -1,6 +1,6 @@
 import { BASE_INSTANCE } from "../../constants";
 
-export const getSingleProductService = async () => {
-  const res = await BASE_INSTANCE.post("/product");
+export const getSingleProductService = async (data) => {
+  const res = await BASE_INSTANCE.post("/product",data);
   return res.data;
 };
