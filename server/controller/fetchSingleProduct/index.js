4c0e98d6fb24e91890ae3efd53ee7f30640ceee1
Mahdi-Ettehadnejad <mahdi.ettehad85@gmail.com>
2023-03-19 07:42:02 +0330

    API to fetch single product is set.

--- server/controller/fetchSingleProduct/index.js
+++ server/controller/fetchSingleProduct/index.js
@@ -1,11 +1,18 @@
-const products = require("../../model/products");
+const Products = require("../../model/products");
 
-const fetchProduct =async (req, res) => {
-  console.log(req.body);
-  const singleproduct= await products.findById('640cf37871a98b0151ed6b8a',{})
- res.json({singleproduct})
-}
+const fetchProduct = async (req, res) => {
+    try {
+      const { id } = req.body;
+      const singleproduct = await Products.findById(id, 'name brand image rating features available price').lean();
+      if (!singleproduct) {
+        return res.status(404).json({ message: "Not found" });
+      }
+      return res.status(200).json({ product: singleproduct });
+    } catch (error) {
+      return res.status(500).json({ message: "Internal server error" });
+    }
+};
 
 module.exports = {
-  fetchProduct,
-}
+    fetchProduct
+};
